15446abf185c167a06976ee6de94aa17bf82e766
razel-xx <razelgpat@gmail.com>
2026-03-31 09:51:56 +0800

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -48,6 +48,6 @@ if ingredient_list:
 
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
 
